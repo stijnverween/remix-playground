ff5a048f1664f0587b32a47e9ad0f5b91ff45004
Stijn Verween <stijn.verween@gmail.com>
2024-05-01 22:50:31 +0200

    chore: update metadata

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,8 +3,8 @@ import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Remix playground" },
+    { name: "description", content: "Welcome to the Remix playground" },
   ];
 };
 
